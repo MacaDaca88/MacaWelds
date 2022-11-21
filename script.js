d42ef2e9a38b3df28bad809559adaa4f73b55068
Michael Duncan <114211123+MacaDaca88@users.noreply.github.com>
2022-11-21 23:04:35 +1300

    updates

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ function myFunction(hover) {
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
 }
+
+function clickCounter() {
+  if (typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+  }
+}
+
+screen.addEventListener("orientationchange", function () {
+  console.log("The orientation of the screen is: " + screen.orientation);
+});
